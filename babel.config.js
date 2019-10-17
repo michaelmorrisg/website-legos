628eb139048144abb9e8b643c5b370ef8dbff7bb
Mike Morris <michaelmorrisg@gmail.com>
2019-10-16 20:05:54 -0600

    tests

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,24 @@ module.exports = function (api) {
 
 	const presets = [ "@babel/preset-env", "@babel/preset-react" ];
 	const plugins = [ ];
+	const module = {
+		rules: [
+			{
+                test: /\.*css$/,
+                use : ExtractTextPlugin.extract({
+                    fallback : 'style-loader',
+                    use : [
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                })
+            },
+		]
+	}
 
 	return {
 		presets,
-		plugins
+		plugins,
+		module
 	};
 };
